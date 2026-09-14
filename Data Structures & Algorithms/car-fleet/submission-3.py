783a4217bc3112764948307d646da8027921255f
Jason Emsley <emsley@purdue.edu>
2026-09-14 17:37:15 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = []
+        cars = sorted(zip(position, speed), reverse=True)
+        stack = []
+        stack.append(cars[0])
+        i = 1
+        def calcETA(car: List[int]):
+            position = car[0]
+            speed = car[1]
+            return (target - position) / speed
+        while i < len(cars):
+            if calcETA(cars[i]) <= calcETA(stack[-1]):
+                i+=1
+                continue
+            else:
+                stack.append(cars[i])
+            i+=1
+        return len(stack)
